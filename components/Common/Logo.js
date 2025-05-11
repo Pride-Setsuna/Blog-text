1aa8575e31490cf2c36473bd54cea5ebba0cd726
Xinzhe.Gao <gao-yiqian@outlook.com>
2025-05-11 22:38:56 +0900

    更新
    更新

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -1,8 +1,9 @@
 // https://react-svgr.com/playground/
 import * as React from 'react'
+import BLOG from '@/blog.config'
 
 const Logo = (props) => (
-  <img src="/favicon.png" width="30" height="30" alt="Logo" />
+  <img src={`${BLOG.link}/favicon.png`} width="30" height="30" alt="Logo" />
 )
 
 export default Logo
