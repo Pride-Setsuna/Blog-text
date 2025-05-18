3777d8ae815151fd60c2e78a33234deb45c906d2
Xinzhe.Gao <gao-yiqian@outlook.com>
2025-05-18 20:22:56 +0900

    优化用户体验
    修复深色模式实时切换

--- components/NotFound.js
+++ components/NotFound.js
@@ -41,7 +41,7 @@ const Page404 = ({ statusCode }) => {
           <Link
             href='/contact'
             scroll={false}
-            className='inline-block bg-blue-200 dark:bg-blue-700 hover:bg-blue-300 dark:hover:bg-blue-600 text-sm md:text-base font-semibold text-center rounded-lg outline-none px-8 py-3 mb-4'
+            className='inline-block bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-sm md:text-base font-semibold text-center rounded-lg outline-none px-8 py-3 mb-4'
           >
             {t.LAYOUT.NOTICE_BUTTON}
           </Link>
