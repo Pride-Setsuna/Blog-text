231593be1cbe37b4d6dbec3caf384a992df2bf31
Xinzhe.Gao <gao-yiqian@outlook.com>
2025-05-18 15:02:45 +0900

    增加判空处理
    修改 components/Hero/Book.js，现在 NotionRenderer 只有在 blockMap 存在时才会渲染

--- components/Hero/Book.js
+++ components/Hero/Book.js
@@ -24,12 +24,16 @@ const BookHero = ({ blockMap }) => {
     <>
       <div className='container mx-auto flex px-5 py-2 mb-10 md:flex-row flex-col items-center'>
         <div className='flex flex-col md:w-4/5 md:items-start mb-6 md:mb-0 md:text-left'>
-          <NotionRenderer
-            className='md:ml-0'
-            blockMap={blockMap}
-            frontMatter={{}}
-            subPageTitle={null}
-          />
+          {blockMap ? (
+            <NotionRenderer
+              className='md:ml-0'
+              blockMap={blockMap}
+              frontMatter={{}}
+              subPageTitle={null}
+            />
+          ) : (
+            <div>暂无内容</div>
+          )}
         </div>
         <div className='w-1/5 ml-4'>
           <BookOpenIcon className='object-cover object-center text-gray-500 dark:text-gray-300' />
