ba24e583dcdf6d9067dd6c345fbf924921c04159
Xinzhe.Gao <gao-yiqian@outlook.com>
2025-06-03 10:08:13 +0900

    修改主页标题，搜索页展示
    部分逻辑

--- pages/search.js
+++ pages/search.js
@@ -6,11 +6,14 @@ export default function search({ tags, posts }) {
 }
 export async function getStaticProps({ locale }) {
   const posts = await getAllPosts({ onlyNewsletter: false, locale: locale || '' })
+  // 随机打乱所有文章顺序
+  const shuffledPosts = posts.sort(() => Math.random() - 0.5)
+  const limitedPosts = shuffledPosts.slice(0, 10)
   const tags = getAllTagsFromPosts(posts)
   return {
     props: {
       tags,
-      posts
+      posts: limitedPosts
     },
     revalidate: 1
   }
